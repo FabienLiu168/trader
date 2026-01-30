f770bbbcb2cb22bacf74a920c3c43f7dd80804cb
FabienLiu168 <fabienliuliu@gmail.com>
2026-01-30 23:15:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown(
 <style>
 div[data-testid="stAppViewContainer"] > .main { padding-top: 3.2rem; }
 
-.app-title{ font-size:3.0rem;font-weight:900;margin-top:-50px;text-align:center;letter-spacing:0.5px;margin-bottom:2px; }
+.app-title{ font-size:3.0rem;font-weight:900;margin-top:-60px;text-align:center;letter-spacing:0.5px;margin-bottom:2px; }
 .app-subtitle{ font-size:1.0rem;opacity:.8;margin:.45rem 0 1.1rem;text-align:center; }
 
 /* 期貨區段標題 */
