2af1c96761df1c716df5944818c7051629cf938c
FabienLiu168 <fabienliuliu@gmail.com>
2026-02-01 11:54:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import requests
 import pandas as pd
 import streamlit as st
 import io
-
+import asyncio
+import aiohttp
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
